19b36b25ae8e8edc2cba14b8795849de37ad4aa6
lordsaga123 <luisjaimevaz@gmail.com>
2023-08-22 21:43:48 -0300

    PreEntrega1 Vázquez

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import cart from './assets/shopping-cart-lg.jpg'
+
+const CartWidget = () => {
+    return(
+        <div>
+            <img src= {cart} alt ="cart-widget"/>
+            0
+        </div>
+    )
+}
+export default CartWidget
